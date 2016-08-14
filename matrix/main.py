263185a656d4bf1184438437bb4626e8fd096ee6
julianaskubs <julianaskubs@gmail.com>
2016-08-13 21:07:14 -0300

    create draw vertical

--- matrix/main.py
+++ matrix/main.py
@@ -10,12 +10,12 @@ class RunProgram:
     def __init__(self):
         self.matrix = []
         while True:
-            # para padronizar a qtde de espacos, retirar todos os espacos e depois colocar
-            # um espaco so entre cada caracter
+            # para padronizar a qtde de espaços, retirar todos os espaços e depois colocar
+            # um espaço so entre cada caracter
             inp = input()
             inp = inp.replace(' ', '').replace('', ' ')
-            # agora a string contem espaco no inicio e no fim, para tirar esses espacos remontamos
-            # a string pegando da posicao 1 ate a ultima posicao, mas sem incluir a ultima posicao
+            # agora a string inp contem espaços a mais (um no inicio e um no fim). Retiramos ao remontar
+            # a string inp, pegando da posicao 1 ate a ultima posicao, mas sem incluir a ultima posicao
             last_pos = len(inp) - 1
             inp = inp[1:last_pos]  # up to but not including
             self.user_input = inp
@@ -31,12 +31,12 @@ class RunProgram:
     # valida a entrada de acordo com o primeiro caracter da string e chama o metodo correspondente
     def val_input(self):
         if self.user_input[0] == 'I' and len(self.user_input) == 5:
-            self.create_m()
+            self.create()
         elif self.user_input[0] == 'C' and len(self.user_input) == 1:
-            self.clear_m()
+            self.clear()
         elif self.user_input[0] == 'L' and len(self.user_input) == 7:
             self.color_pix()
-        elif self.user_input[0] == 'V':
+        elif self.user_input[0] == 'V' and len(self.user_input) == 9:
             self.draw_vert()
         elif self.user_input[0] == 'H':
             self.draw_horiz()
@@ -48,7 +48,7 @@ class RunProgram:
             self.save_img()
 
     # Cria uma matriz de acordo com os parametros, todas as posicoes contendo valor 0
-    def create_m(self):
+    def create(self):
         try:
             m = int(self.user_input[2])
             n = int(self.user_input[4])
@@ -58,26 +58,40 @@ class RunProgram:
             print('Paramaters not enough to create a matrix')
 
     # Limpa a matriz, percorrendo-a linha a linha
-    def clear_m(self):
+    def clear(self):
         for i, item in enumerate(self.matrix, 0):
             for j, _item in enumerate(item, 0):
                 item[j] = 0
-
-        print('\n'.join([''.join(['{:1}'.format(item) for item in row]) for row in self.matrix]))
+        # print('\n'.join([''.join(['{:1}'.format(item) for item in row]) for row in self.matrix]))
 
     # Colore um pixel, de acordo com as coordenadas
     def color_pix(self):
-        m = int(self.user_input[2])
-        n = int(self.user_input[4])
-        c = self.user_input[6]
-        for i, item in enumerate(self.matrix, 0):
-            # por causa do indice 0, substraimos 1 para colorir na coordenada digitada
-            if i == n - 1:
-                for j, _item in enumerate(item, 0):
-                    if j == m - 1:
-                        item[j] = c
-        print('\n'.join([''.join(['{:1}'.format(item) for item in row]) for row in self.matrix]))
+        try:
+            x = int(self.user_input[2])  # coluna
+            y = int(self.user_input[4])  # linha
+            c = self.user_input[6]
+            for i, item in enumerate(self.matrix, 0):
+                # por causa do indice 0, substraimos 1 para colorir na coordenada correta
+                if i == y - 1:
+                    for j, _item in enumerate(item, 0):
+                        if j == x - 1:
+                            item[j] = c
+            # print('\n'.join([''.join(['{:1}'.format(item) for item in row]) for row in self.matrix]))
+        except:
+            print('Paramaters not enough to update matrix')
 
+    # Desenha um segmento vertical na coluna x, da linha y1 ate a linha y2
+    def draw_vert(self):
+        x = self.user_input[2]  # coluna
+        y1 = self.user_input[4]  # linha
+        y2 = self.user_input[6]  # linha
+        c = self.user_input[8]
+        for i, item in enumerate(self.matrix, 0):
+            if y1 - 1 <= i <= y2 - 1:  # se i estiver entre o range de linhas y1 e y2
+                for j, _item in enumerate(item, 0):
+                    if j == x - 1:  # se a coluna for igual a x
+                        item[j] = c
+        # (subtraimos 1 por causa do indice 0)
 
 
 if __name__ == '__main__':
